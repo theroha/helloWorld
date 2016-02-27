a365c6a4864bf3b4cfafaa25ed5b3d44aced4b40
theroha <theroha@yahoo.com>
2016-02-27 11:55:44 -0600

    Added files via upload

---
+++ lights.py
@@ -0,0 +1,23 @@
+#import dependencies
+import pyxhook #required for hook
+from pygame import midi	#required for midi
+
+midi.init()
+
+output = midi.Output(2)
+
+def OnKeyPress(event):
+	if event.Ascii == 97: #Virtual Key Id of right arrow key
+		output.write_short(0xc0, 0)
+		
+	if event.Ascii == 96:
+		new_hook.cancel()
+
+#instantiate HookManager class
+new_hook=pyxhook.HookManager()
+#listen to all keystrokes
+new_hook.KeyDown=OnKeyPress
+#hook the keyboard
+new_hook.HookKeyboard()
+#start the session
+new_hook.start()
